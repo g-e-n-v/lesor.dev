4a52ee804fac25850682fb5a5a8b2023f6199033
luc.le <luc.le@enouvo.com>
2024-05-19 22:11:20 +0700

    responsive

--- app/(main-layout)/layout.tsx
+++ app/(main-layout)/layout.tsx
@@ -1,11 +1,17 @@
 import type { PropsWithChildren } from "react";
 
 import { Sidebar } from "@/components/Sidebar";
+import { cn } from "@/utils/cn.util";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="mx-auto flex h-screen max-w-6xl gap-x-10 px-4">
-      <Sidebar className="h-full min-w-52 shrink-0" />
+    <div className={cn("mx-auto flex h-screen max-w-6xl flex-col gap-x-10 px-4", "md:flex-row")}>
+      <Sidebar
+        className={cn(
+          "fixed left-0 top-0 z-10 h-screen w-screen shrink-0 bg-neutral-900 px-4",
+          "md:static md:h-full md:w-60"
+        )}
+      />
       <main className="h-full grow overflow-y-auto">{children}</main>
     </div>
   );
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,13 +3,16 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 import BEAR_PINK_GIF from "@/assets/images/bear-pink.gif";
 import AVATAR_IMAGE from "@/assets/images/trungluc.jpg";
 import IconRight from "@/assets/svgs/arrow-right.svg";
 import IconBook from "@/assets/svgs/book.svg";
+import IconClose from "@/assets/svgs/close.svg";
 import IconHeartbeat from "@/assets/svgs/heartbeat.svg";
 import IconHome from "@/assets/svgs/home.svg";
+import IconMenu from "@/assets/svgs/menu.svg";
 import IconSearch from "@/assets/svgs/search.svg";
 import IconSend from "@/assets/svgs/send.svg";
 import { KeyboardKey } from "@/components/KeyboardKey";
@@ -46,58 +49,87 @@ type SidebarProps = {
 export function Sidebar({ className }: SidebarProps) {
   const pathname = usePathname();
 
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className={className}>
-      <aside className="flex h-full flex-col pb-4 pt-10">
-        <div className="mb-8 flex gap-4">
-          <Image className="size-14 rounded-full" src={AVATAR_IMAGE} alt="luc.le" />
-          <div className="flex flex-col justify-evenly">
-            <h1 className="flex items-center gap-2 text-xl font-bold">
-              Luc Le <Image src={BEAR_PINK_GIF} alt="thunderbolt-gif" className="size-6" />
-            </h1>
-            <h2 className="font-medium text-neutral-500">@lesor_</h2>
+    <>
+      <button
+        className={cn(
+          "fixed right-4 top-4 z-50 cursor-pointer rounded-full border-[1.5px] border-solid border-neutral-100 bg-neutral-800 p-2 text-2xl",
+          "md:hidden"
+        )}
+        onClick={() => {
+          setIsOpen((isOpen) => !isOpen);
+        }}
+      >
+        {isOpen ? <IconClose /> : <IconMenu />}
+      </button>
+
+      <header
+        className={cn(
+          "transition duration-500",
+          className,
+          {
+            "translate-x-full": !isOpen,
+          },
+          "md:transform-none"
+        )}
+      >
+        <aside className="flex h-full flex-col pb-4 pt-10">
+          <div className="mb-8 flex items-center gap-4">
+            <Image className="size-14 rounded-full" src={AVATAR_IMAGE} alt="luc.le" />
+
+            <div className="flex grow flex-col justify-evenly">
+              <h1 className="flex items-center gap-2 text-xl font-bold">
+                Luc Le <Image src={BEAR_PINK_GIF} alt="thunderbolt-gif" className="size-6" />
+              </h1>
+              <h2 className="font-medium text-neutral-500">@lesor_</h2>
+            </div>
           </div>
-        </div>
 
-        <button className="mb-12 flex w-full items-center gap-2 rounded-lg border-2 border-solid border-neutral-700 p-2 text-neutral-500">
-          <IconSearch />
-          <span className="grow text-left">Search</span>
-          <KeyboardKey>⌘ K</KeyboardKey>
-        </button>
+          <button className="mb-12 flex w-full items-center gap-2 rounded-lg border-2 border-solid border-neutral-700 p-2 text-neutral-500">
+            <IconSearch />
+            <span className="grow text-left">Search</span>
+            <KeyboardKey>⌘ K</KeyboardKey>
+          </button>
 
-        <nav className="grow">
-          <ul className="flex flex-col gap-2">
-            {NAV_ITEMS.map((item) => {
-              const isActive = pathname.includes(item.href);
+          <nav className="grow">
+            <ul className="flex flex-col gap-2">
+              {NAV_ITEMS.map((item) => {
+                const isActive = pathname.includes(item.href);
 
-              return (
-                <li key={item.label}>
-                  <Link
-                    href={item.href}
-                    className={cn(
-                      "relative flex items-center gap-2 rounded-lg p-2 transition",
-                      "hover:bg-neutral-700/50"
-                    )}
-                  >
-                    {item.icon}
-                    <span className="grow">{item.label}</span>
-                    {isActive && <IconRight className="text-neutral-500" />}
+                return (
+                  <li key={item.label}>
+                    <Link
+                      onClick={() => {
+                        setIsOpen(false);
+                      }}
+                      href={item.href}
+                      className={cn(
+                        "relative flex items-center gap-2 rounded-lg p-2 transition",
+                        "hover:bg-neutral-700/50"
+                      )}
+                    >
+                      {item.icon}
+                      <span className="grow">{item.label}</span>
+                      {isActive && <IconRight className="text-neutral-500" />}
 
-                    {isActive && (
-                      <motion.div
-                        layoutId="active-route"
-                        className="absolute left-0 top-0 -z-10 size-full rounded-lg bg-neutral-700"
-                      />
-                    )}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
+                      {isActive && (
+                        <motion.div
+                          layoutId="active-route"
+                          className="absolute left-0 top-0 -z-10 size-full rounded-lg bg-neutral-700"
+                        />
+                      )}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
 
-        {/* <ThemeSwitcher /> */}
-      </aside>
-    </header>
+          {/* <ThemeSwitcher /> */}
+        </aside>
+      </header>
+    </>
   );
 }
