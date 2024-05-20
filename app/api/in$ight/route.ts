56de1908f768575210d7e1e479b07b5e64d473f9
luc.le <luc.le@enouvo.com>
2024-05-21 00:09:16 +0700

    obfuscate umami script

---
+++ app/api/in$ight/route.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const ORIGINAL_SCRIPT_URL = "https://cloud.umami.is/script.js";
+// const ORIGINAL_API_ENDPOINT = "https://api-gateway.umami.dev";
+
+// const OBFUSCATED_API_ENDPOINT = "/";
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+};
+
+export async function GET(req: NextRequest) {
+  const { data: originalScript, status } = await axios.get<string>(ORIGINAL_SCRIPT_URL, {
+    headers: {
+      ...(req.headers as unknown as Record<string, string>),
+      ...CORS_HEADERS,
+      "accept-encoding": "gzip",
+      host: null,
+    },
+    decompress: true,
+  });
+
+  const obfuscatedScript = originalScript;
+
+  return new NextResponse(obfuscatedScript, { status });
+}
