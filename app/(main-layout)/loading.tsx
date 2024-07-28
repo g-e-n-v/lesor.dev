3b5685ac8a9fba0f8cc8f39eefd002b012864120
luc.le <trungluc.dev@gmail.com>
2024-07-28 23:50:02 +0700

    update loader

--- app/(main-layout)/loading.tsx
+++ app/(main-layout)/loading.tsx
@@ -1,3 +1,12 @@
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div className="animate-spin grid w-max grid-cols-2 gap-1">
+        <i className="w-3 h-3 rounded-full bg-neutral-500" />
+        <i className="w-3 h-3 rounded-full bg-neutral-500/75" />
+        <i className="w-3 h-3 rounded-full bg-neutral-500/50" />
+        <i className="w-3 h-3 rounded-full bg-neutral-500/25" />
+      </div>
+    </div>
+  );
 }
